a23d388f5f569b9953cd5e95afdf83abd6669413
MOHAMED N BADAWY <m.m.nabel@gmail.com>
2020-08-26 22:42:11 -0400

    Update SudokuTest.java

--- src/test/SudokuTest.java
+++ src/test/SudokuTest.java
@@ -4,8 +4,8 @@ import jdk.nashorn.internal.ir.annotations.Ignore;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import sodukovalidator.Main;
-import sodukovalidator.Sudoku;
+import validator.Main;
+import validator.Sudoku;
 
 import java.io.*;
 import java.lang.reflect.Method;
